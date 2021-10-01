334d8333c7a2f24ff06e1cf9c58f19c1ae112a67
Victor Souza <victor.augusto800@gmail.com>
2021-10-01 18:10:34 -0300

    Add toggle funcionability for increase and decrease

--- src/context.js
+++ src/context.js
@@ -36,6 +36,10 @@ const AppProvider = ({ children }) => {
     dispatch({ type: "DISPLAY_ITEMS", payload: cart });
   };
 
+  const toggleAmount = (id, type) => {
+    dispatch({ type: "TOGGLE_AMOUNT", payload: { id, type } });
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -51,6 +55,7 @@ const AppProvider = ({ children }) => {
         remove,
         increase,
         decrease,
+        toggleAmount,
       }}
     >
       {children}
